72828ddb626ca6c978dd28aadf8123ce18ed14b0
sierraskyewallace <sskwallace@gmail.com>
2021-07-25 11:28:04 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  addTask();
+})
+
+function addTask() {
+  const form  = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const task = document.getElementById("new-task-description").value;
+    const list = document.getElementById("tasks");
+    const item = document.createElement("li");
+    item.innerText = task;
+    list.appendChild(item);
+  })  
+}
